ccfc4250895d19009f39821464f74eaf3fe00729
Shumiya Tomohide <63492321+Tom-Shumi@users.noreply.github.com>
2025-10-29 23:08:03 +0900

    fix typo

--- api/migrate_cloud_db.py
+++ api/migrate_cloud_db.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext import InternalError, OperationalError
+from sqlalchemy.exc import InternalError, OperationalError
 from sqlalchemy import create_engine
 
 from api.models.task import Base as TaskBase
@@ -10,6 +10,7 @@ DEMO_DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/demo
 
 engine = create_engine(DEMO_DB_URL, echo=True)
 
+
 def database_exists():
     try:
         engine.connect()
@@ -18,14 +19,17 @@ def database_exists():
         print(f"Database connection error: {e}")
         return False
 
+
 def create_database():
     if not database_exists():
         root = create_engine(DB_URL, echo=True)
         with root.connect() as conn:
-            conn.execute("CREATE DATABASE demo CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;")
+            conn.execute(
+                "CREATE DATABASE demo CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;")
         print("Database 'demo' created.")
         TaskBase.metadata.create_all(engine)
         UserBase.metadata.create_all(engine)
+
 
 if __name__ == "__main__":
     create_database()
